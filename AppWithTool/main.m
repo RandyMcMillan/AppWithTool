b7240e6fbbf766cf42cd1eb298c8c45f0de91ac7
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-05-16 19:17:18 -0400

    AppWithTool/main.m: posix execute commands

--- AppWithTool/main.m
+++ AppWithTool/main.m
@@ -5,7 +5,18 @@
 //  Created by git on 4/25/23.
 //
 
+//REF: https://developer.apple.com/forums/thread/106590
 #import <Cocoa/Cocoa.h>
+
+int execv(const char *path, char *const argv[]);
+int execve(const char *path, char *const argv[], char *const envp[]);
+int execvp(const char *file, char *const argv[]);
+int execvpe(const char *file, char *const argv[], char *const envp[]);
+
+int execv(const char *path, char *const argv[]){ return 0; }
+int execve(const char *path, char *const argv[], char *const envp[]){ return 0; }
+int execvp(const char *file, char *const argv[]){ return 0; }
+int execvpe(const char *file, char *const argv[], char *const envp[]){ return 0; }
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
