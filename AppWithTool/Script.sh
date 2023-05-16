0d8d4624519bff5a22687a054c89197ccfa35883
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-05-16 19:45:29 -0400

    clean up

--- AppWithTool/Script.sh
+++ AppWithTool/Script.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-
-#  Script.sh
-#  AppWithTool
-#
-#  Created by git on 5/16/23.
-#  
-# echo $(whoami)
 echo $(PWD)
 echo "/Users/"$(whoami)"/Library/Containers/dev.bitcoincore.AppWithTool/Data"
