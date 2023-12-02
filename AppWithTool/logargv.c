8c2b4ad27c21827597cd032fb699c6b28e60d4e2
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-12-02 15:24:13 -0500

    AppWithTool/logargv.c

--- AppWithTool/logargv.c
+++ AppWithTool/logargv.c
@@ -9,14 +9,18 @@
 
 int logargv(int *count, char *argv[]){
 
+    printf("logargv:begin\n");
     int argvlen = 0;
     int i = *count;
+    printf("while(argv[argvlen] != NULL):\n");
     while(argv[argvlen] != NULL){
         while(argv[*count-i] != NULL){
         printf("i=%d\n",i);
         printf("%s\n", argv[*count-i]);
         i--;
         }
+    printf("1.logargv:end\n");
     }
+    printf("2.logargv:end\n");
     return 0;
 }
