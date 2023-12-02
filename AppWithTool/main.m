bd47b694a8a2574251cc78e3db044ab7ad09060b
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-12-02 15:23:23 -0500

    AppWithTool/main.m

--- AppWithTool/main.m
+++ AppWithTool/main.m
@@ -31,7 +31,39 @@ int execve(const char *path, char *const argv[], char *const envp[]){ return 0; 
 int execvp(const char *file, char *const argv[]){ return 0; }
 int execvpe(const char *file, char *const argv[], char *const envp[]){ return 0; }
 
-void runScript(NSString* scriptName)
+void privateFrameworksPath(NSString* scriptName)
+{
+    NSTask *task;
+    task = [[NSTask alloc] init];
+    [task setLaunchPath: @"/bin/sh"];
+
+    NSArray *arguments;
+    NSString* newpath = [NSString stringWithFormat:@"%@/%@",[[NSBundle mainBundle] privateFrameworksPath], scriptName];
+    //NSString* newpath = [NSString stringWithFormat:@"%@/%@",[[NSBundle mainBundle] sharedSupportPath], scriptName];
+    NSLog(@"shell script path: %@", newpath);
+    arguments = [NSArray arrayWithObjects:newpath, nil];
+    [task setArguments: arguments];
+
+    NSPipe *pipe;
+    pipe = [NSPipe pipe];
+    [task setStandardOutput: pipe];
+
+    NSFileHandle *file;
+    file = [pipe fileHandleForReading];
+
+    [task launch];
+
+    NSData *data;
+    data = [file readDataToEndOfFile];
+
+    NSString *string;
+    string = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+    NSLog (@"script returned:\n%@", string);
+}
+
+
+
+void runScriptSharedSupport(NSString* scriptName)
 {
     NSTask *task;
     task = [[NSTask alloc] init];
@@ -40,7 +72,7 @@ void runScript(NSString* scriptName)
     NSArray *arguments;
     //NSString* newpath = [NSString stringWithFormat:@"%@/%@",[[NSBundle mainBundle] privateFrameworksPath], scriptName];
     NSString* newpath = [NSString stringWithFormat:@"%@/%@",[[NSBundle mainBundle] sharedSupportPath], scriptName];
-    NSLog(@"shell script path: %@",newpath);
+    NSLog(@"shell script path: %@", newpath);
     arguments = [NSArray arrayWithObjects:newpath, nil];
     [task setArguments: arguments];
 
@@ -63,16 +95,26 @@ void runScript(NSString* scriptName)
 
 int main(int argc, char const *argv[]) {
     @autoreleasepool {
+        NSLog (@"main");
         int *count=&argc;
-        logargc(count);
-        //argv[argc] = "test";//(int *)argc;
+        NSLog (@"logargc(count)");
+        int returncount = logargc(count);
+        NSLog (@"returncount = logargc(count) = %d", returncount);
+
+        argv[argc] = "ToolX";//(int *)argc;
         //logargc(count);
         //logargc(&argc);
+        
         //int success = logargv(&argc, (char **)argv);//blocking
-        //if(success){printf("%d",success);};
-        //execv_bridge(&argc,(char *const *)argv);
-        //execcl_bridge(*count,(char *const *)argv);
-        runScript(@"Script.sh");
+        //if(success){printf("success=%d",success);};
+        //if(!success){printf("!success=%d",success);};
+        
+        execv_bridge((const char *)argv, (char **)argv);
+        //int execcl_bridge(int argc, char* argv[]);
+        execcl_bridge(argc,(char **)argv);
+        
+        runScriptSharedSupport(@"template.sh");
+        runScriptSharedSupport(@"Script.sh");
 
     }
     return NSApplicationMain(argc, argv);
