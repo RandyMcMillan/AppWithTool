a4d05c12c8f86c58e387711fb128de3d083ca7bd
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-12-02 15:23:44 -0500

    AppWithTool/execcl_bridge.c

--- AppWithTool/execcl_bridge.c
+++ AppWithTool/execcl_bridge.c
@@ -31,11 +31,42 @@ int execcl_bridge(int argc, char* argv[]){
     sprintf(buffer, "%s -l %s", LS_EXEC, argv[1]);
     printf("buffer: %s\n", buffer);
     //build the argument vector
+    sprintf(buffer, "%s -l %s", LS_EXEC, "");
+    printf("buffer: %s\n", buffer);
 
     //execute the command
-    if(execl(BASH_EXEC, BASH_EXEC, "-c", buffer, NULL) < 0){
-        printf("execcl_bridge:Something terrible happended!");
-        return 1;
+    printf("BASH_EXEC: %s\n", BASH_EXEC);
+    //int     execl(const char * __path, const char * __arg0, ...) __WATCHOS_PROHIBITED __TVOS_PROHIBITED;
+    //if(execl(BASH_EXEC, BASH_EXEC, "-c", buffer, NULL) < 0){
+    
+    printf("argv[0]: %s\n", argv[0]);
+    printf("argv[1]: %s\n", argv[1]);
+    printf("argv[2]: %s\n", argv[2]);
+    
+    if(execl(BASH_EXEC, "echo 1") > 0){
+        printf("> 0:bash:xecl=%d\n", execl(BASH_EXEC, "echo"));
+        printf("> 0:bash:execcl_bridge:Something terrible happended!\n");
+        char pwd = execl(BASH_EXEC, "echo `pwd`");
+        printf("> 0:bash:pwd=%d\n", pwd);
+        //return execl(BASH_EXEC, "echo 1");
+    }else{
+        printf("! > 0:bash:execl=%d\n", execl(BASH_EXEC, "echo 0"));
+        char pwd = execl(BASH_EXEC, "echo `pwd`");
+        printf("! > 0:bash:pwd=%d\n", pwd);
+        //return execl(BASH_EXEC, "echo 0");
+    }
+
+    if(execl(LS_EXEC, "echo 1") > 0){
+        printf("> 0:ls:execl=%d\n", execl(LS_EXEC, "-l"));
+        printf("> 0:ls:execcl_bridge:Something terrible happended!\n");
+        char pwd = execl(LS_EXEC, "-l");
+        printf("> 0:ls:pwd=%d\n", pwd);
+        //return execl(LS_EXEC, "echo 1");
+    }else{
+        printf("! > 0:ls:execl=%d\n", execl(LS_EXEC, "-l"));
+        char pwd = execl(LS_EXEC, "-l");
+        printf("! > 0:ls:pwd=%d\n", pwd);
+        //return execl(LS_EXEC, "echo 0");
     }
 
     return 0;
